3d446499676031eb32882f5c9209f177bcc74f6d
kritiparash <kritiparashar0606@gmail.com>
2024-02-02 00:12:26 +0530

    helloall

---
+++ app2.java
@@ -0,0 +1,12 @@
+public class MYapp {
+
+    public static void main(String[]args){
+        System.out.println("Java app pulled and build by jenkins");
+        System.out.println("Hello java and jenkins");
+        System.out.println("Hello java and jenkins2");
+        
+
+
+    }
+    
+}
