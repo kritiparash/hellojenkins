764e0e0ff451793f07114b47792bfe845bdb7899
kritiparash <kritiparashar0606@gmail.com>
2024-02-02 08:45:59 +0530

    hello git1

--- app2.java
+++ app2.java
@@ -1,10 +1,10 @@
-public class MYapp {
+public class app2 {
 
     public static void main(String[]args){
         System.out.println("Java app pulled and build by jenkins");
         System.out.println("Hello java and jenkins");
         System.out.println("Hello java and jenkins2");
-        System.out.println("jenkins2");
+        
 
         
 
